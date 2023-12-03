12650c3457221fdf385b1c25260d3e55e986fb2e
Laix <894169287@qq.com>
2023-12-03 21:51:30 +0800

    remove searchbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
 import myPlugin from './myPlugin/my-plugin.js'
 
 
@@ -16,7 +15,6 @@ export default defineUserConfig({
     theme: defaultTheme({
         // 默认主题配置
         navbar: [
-            // NavbarItem
             {
                 text: '首页',
                 link: '/',
@@ -29,28 +27,11 @@ export default defineUserConfig({
                 text: '关于',
                 link: '/about.md'
             }
-            // NavbarGroup
-            // {
-            //     text: '测试',
-            //     children: ['/test/index.md', '/test/malou.md'],
-            // },
-            // 字符串 - 页面文件路径
-            // '/README.md',
         ],
-        // sidebar: {
-        //     '/test/': [
-
-        //     ]
-        // },
-        logo: '/favicon.ico',
         repo: 'https://github.com/Laic7092/blog',
         editLink: false
     }),
     plugins: [
-        myPlugin(),
-        searchPlugin({
-            isSearchable: (page) => page.path !== '/',
-            getExtraFields: (page) => [page.frontmatter.tags, page.date] as Array<string>
-        })
+        myPlugin()
     ]
 })
